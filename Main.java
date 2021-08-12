974e93432589ea8a6d70c28c2c63fa0028aa6a0e
Saleh Khan <saleh.lab49@gmail.com>
2021-08-12 23:35:12 +0530

    Fixes for in.close()

--- Main.java
+++ Main.java
@@ -5,33 +5,25 @@ import java.util.*; // contains Collections framework
 class Main {
 	public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
-        String x = in.nextLine().trim();
-        String arr = in.nextLine().trim();
-		in.close();
-        /*Convert to integer*/
-        int n = Integer.parseInt(x);
-        /*Splitting the string by space and storing it as an array*/
-        String[] splitStr = arr.split("\\s+");
-        //System.out.println(splitStr);
+		try{
+			String x = in.nextLine().trim();
+        	String arr = in.nextLine().trim();
+        	/*Convert to integer*/
+        	int n = Integer.parseInt(x);
+        	/*Splitting the string by space and storing it as an array*/
+        	String[] splitStr = arr.split("\\s+");
+        	//System.out.println(splitStr);
         
-        int[] numbers = new int[n+1];
-        for(int i = 0; i < n; i++) {
-            numbers[i+1] = Integer.parseInt(splitStr[i]);
-        }
-        int sum = 0;
-        int mul = 1;
+        	int[] numbers = new int[n+1];
+        	for(int i = 0; i < n; i++) {
+            	numbers[i] = Integer.parseInt(splitStr[i]);
+        	}
+        	for(int i = 0; i < n; i++){
+				System.out.printf("%d ", numbers[i]);
+			}
 
-        for(int i = 1; i <= n; i++){
-            if(i%2 == 0){
-                sum += numbers[i];
-            } else if(i%2 != 0){
-                mul *= numbers[i];
-            }
-        }
-        System.out.printf("%d %d",sum, mul);
-        //System.out.println(Arrays.toString(numbers));
-        
-
-        
+		} finally{
+			in.close();
+		}
 	}
 }
